1e679c4da17bb3378ad3820631b858c15ef06a99
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2023-11-03 20:33:34 +0700

    update ppt and upload file

--- SpringMVC_thuc_tap/src/main/java/com/example/springmvc_thuc_tap/controller/orderController.java
+++ SpringMVC_thuc_tap/src/main/java/com/example/springmvc_thuc_tap/controller/orderController.java
@@ -2,17 +2,29 @@ package com.example.springmvc_thuc_tap.controller;
 
 import com.example.springmvc_thuc_tap.model.Order;
 import com.example.springmvc_thuc_tap.service.orderService;
+
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/order")
@@ -38,5 +50,46 @@ public class orderController {
 //        System.out.println(name +   address +  phone +  orderName);
         orderServ.registerOrder(name,  address, phone, orderName);
         return "redirect:/order";
+    }
+
+    @GetMapping("getUploadFile")
+    public String getUploadFile(){return "upload_file";}
+
+//    @Value("${upload.directory}")
+//    private Resource uploadDirectory;
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file,
+                             @RequestParam("status") String status) throws Exception {
+//        Path uploadPath = Paths.get(uploadDirectory.getURI());
+        // Get the original filename
+        String originalFilename = file.getOriginalFilename();
+
+        String projectDir = System.getProperty("user.dir");
+        // Define the upload directory
+        String uploadDir = "";
+        if (status.equals("public")){
+            uploadDir = projectDir + "\\src\\main\\resources\\static\\uploads";
+        } else{
+            uploadDir = projectDir + "\\src\\main\\uploads";
+        }
+
+        System.out.println(uploadDir);
+        // Create the upload path if it doesn't exist
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        if (file.getSize() > 5000000) { // Maximum file size of 5MB
+            throw new Exception("File size too large. Please upload a file less than 5MB.");
+        }
+        // Create a new file with a unique name in the upload directory
+        File uploadedFile = new File(uploadDir, originalFilename);
+        // Save the uploaded file to disk
+        try (InputStream inputStream = file.getInputStream()) {
+            Files.copy(inputStream, uploadedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+        return "redirect:/order/getUploadFile"; // Redirect to another page after file upload
     }
 }
